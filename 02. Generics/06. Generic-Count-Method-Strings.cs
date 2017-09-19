764274d73d7a5bfa5cc2c56a3255422e695c52d3
Vania Dimitrova <vani4ka66@abv.bg>
2017-09-19 13:48:28 +0300

    Create 06. Generic-Count-Method-Strings.cs

---
+++ 02. Generics/06. Generic-Count-Method-Strings.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace August2017
+{
+
+    public class Box<T> where T : IComparable<T>
+    {
+        private T value;
+
+        public Box()
+            : this(default(T))
+        {
+
+        }
+
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+        public T Value { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box<string>> boxes = new List<Box<string>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                boxes.Add(new Box<string>(Console.ReadLine()));
+            }
+            Box<string> comparableBox = new Box<string>(Console.ReadLine());
+
+            int countOfGreaterElements = CompareElements(boxes, comparableBox);
+
+            Console.WriteLine(countOfGreaterElements);
+        }
+
+        static int CompareElements<T>(List<Box<T>> boxes, Box<T> comparaableBox)
+            where T : IComparable<T>
+        {
+            int count = 0;
+            foreach (var item in boxes)
+            {
+                if (item.Value.CompareTo(comparaableBox.Value) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
+
